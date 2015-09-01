d11665b1af3b3c0e9fe9790844fa32ac0a2f2021
IrinaCodixis <ijumamuradova@codixis.com>
2015-09-01 15:19:16 +0300

    add data fixtures

---
+++ DataFixtures/ORM/LoadEventData.php
@@ -0,0 +1,26 @@
+<?php
+namespace Test\EventBundle\DataFixtures\ORM;
+ 
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Test\EventBundle\Entity\Guestss;
+ 
+class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
+{
+  public function load(ObjectManager $em)
+  {
+    $guest_one = new Guestss();
+    $guest_one->setName('John Smith');
+    $guest_one->setEmail('john.smith@example.com');
+    $guest_one->setIsAttending(true);
+ 
+     
+    $em->persist($guest_one);
+     
+    $em->flush();
+  }
+ 
+}
+
+?>
